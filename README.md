a41c5d02bb1801e7785b6de7dd0bd9327d499cd2
Tomas Trebicky <tomas@adamnet.works>
2023-05-19 11:58:32 -0400

    Initial commit.

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # nvim-tabline
 
+Created as a fork of [crispgm/nvim-tabline](https://github.com/crispgm/nvim-tabline) with the following main improvement in mind:
+
+* Support for scrolling through a multitude of tabs.
+
 A minimal Tabline plugin for Neovim, written in Lua.
 It is basically a drop-in replacement for [tabline.vim](https://github.com/mkitt/tabline.vim) but with [a few differences](#Differences).
 
@@ -11,7 +15,7 @@ With `lazy.nvim`:
 
 ```lua
 {
-    'crispgm/nvim-tabline',
+    'ethician/nvim-tabline',
     dependencies = { 'nvim-tree/nvim-web-devicons' }, -- optional
     config = true,
 }
@@ -30,11 +34,26 @@ require('tabline').setup({
     show_index = true,           -- show tab index
     show_modify = true,          -- show buffer modification indicator
     show_icon = false,           -- show file extension icon
+    show_scrollers = true,       -- show left/right scroller indicators
+    show_nr_tabs = true,         -- show total number of tabs
+    shorten_path = false,        -- directory is represented by a `shorten_length` number of characters
+    shorten_path_fully = false,  -- directory substructure is represented by `shorten_indicator`
+    shorten_indicator = '_',     -- self descriptive
+    separator = ' ',             -- self descriptive
     fnamemodify = ':t',          -- file name modifier
-    modify_indicator = '[+]',    -- modify indicator
-    no_name = 'No name',         -- no name buffer name
-    brackets = { '[', ']' },     -- file name brackets surrounding
-    inactive_tab_max_length = 0  -- max length of inactive tab titles, 0 to ignore
+    scroller_left = '< ',        -- left scroller
+    scroller_right = ' >',       -- right scroller
+    no_name = '[No Name]',       -- no-name buffer name
+    header = '',                 -- static string at the beginning of a tabline
+    footer = '',                 -- static string at the end of a tabline
+    prefix = '',                 -- static string at the beginning of a tab
+    suffix = '',                 -- static string at the end of a tab
+    active_prefix = '',          -- static string at the beginning of an active tab
+    active_suffix = '',          -- static string at the end of an active tab
+    modify_indicator = '+',      -- inidicator of a modified buffer
+    shorten_length = 1,          -- see `shorten_path`
+    tab_max_length = 0,          -- max length of a tab
+    filename_max_length = 0,     -- max filename length before it gets cut off and prepended by `shorten_indicator`
 })
 ```
 
--- lua/tabline.lua
+++ lua/tabline.lua
@@ -4,92 +4,397 @@
 local M = {}
 local fn = vim.fn
 
+local utf8 = require('utf8')
+
 M.options = {
-    show_index = true,
-    show_modify = true,
-    show_icon = false,
-    fnamemodify = ':t',
-    brackets = { '[', ']' },
-    no_name = 'No Name',
-    modify_indicator = ' [+]',
-    inactive_tab_max_length = 0,
+  show_index = true,
+  show_modify = true,
+  show_icon = false,
+  show_scrollers = true,
+  show_nr_tabs = true,
+  shorten_path = false,
+  shorten_path_fully = false,
+  shorten_indicator = '_',
+  separator = ' ',
+  fnamemodify = ':t',
+  scroll_left = '< ',
+  scroll_right = ' >',
+  no_name = '[No Name]',
+  header = '',
+  footer = '',
+  prefix = '',
+  suffix = '',
+  active_prefix = '',
+  active_suffix = '',
+  modify_indicator = '+',
+  shorten_length = 1,
+  tab_max_length = 0,
+  filename_max_length = 0,
 }
 
-local function tabline(options)
-    local s = ''
-    for index = 1, fn.tabpagenr('$') do
-        local winnr = fn.tabpagewinnr(index)
-        local buflist = fn.tabpagebuflist(index)
-        local bufnr = buflist[winnr]
-        local bufname = fn.bufname(bufnr)
-        local bufmodified = fn.getbufvar(bufnr, '&mod')
+M.consts = {
+  path_separator = function()
+    if vim.fn.has("win32") == 1 or vim.fn.has("win32unix") == 1 then
+      return '\\'
+    else
+      return '/'
+    end
+  end,
+  separator_length = 0,
+  scroll_left_length = 0,
+  scroll_right_length = 0,
+  header_length = 0,
+  footer_length = 0,
+  unavailable_length = 0,
+}
 
-        s = s .. '%' .. index .. 'T'
-        if index == fn.tabpagenr() then
-            s = s .. '%#TabLineSel#'
+M.pages = {
+  count = 0,
+  active_tab_index = 0,
+  previous_active_tab_index = 0,
+  view_begin_index = 0,
+  view_end_index = 0,
+  alignment = 0,  -- 0 left, 1 right
+  have_left_scroller = false,
+  have_right_scroller = false,
+  tabs = {},
+}
+
+function M.pages:get(index)
+  local tab = self.tabs[index]
+  if tab == nil then
+    tab = {
+      index = index,
+      previous_index = index,
+      is_processed = false,
+      is_active = false,
+      absolute_begin_index = 0,
+      absolute_end_index = 0,
+      string = '',
+      string_length = 0,
+    }
+    table.insert(self.tabs, tab)
+    self.count = self.count + 1
+  else
+    if tab.index ~= index then
+      if self.previous_active_tab_index == tab.index then
+        self.previous_active_tab_index = index
+      end
+      tab.previous_index = tab.index
+      tab.index = index
+    end
+  end
+  return tab
+end
+
+function M.pages:get_active()
+  if self.active_tab_index == 0 then
+    return nil
+  else
+    return self.tabs[self.active_tab_index]
+  end
+end
+
+local function BuildBufferName(tab)
+  local winnr = fn.tabpagewinnr(tab.index)
+  local buflist = fn.tabpagebuflist(tab.index)
+  local bufnr = buflist[winnr]
+  local bufname = fn.bufname(bufnr)
+  local bufmodified = fn.getbufvar(bufnr, '&mod')
+
+  tab.string = ''
+
+  -- active
+  if tab.index == fn.tabpagenr() then
+    M.pages.previous_active_tab_index = M.pages.active_tab_index
+    M.pages.active_tab_index = tab.index
+    tab.is_active = true
+    tab.string = tab.string .. M.options.active_prefix
+  else
+    tab.is_active = false
+    tab.string = tab.string .. M.options.prefix
+  end
+
+  -- tab index
+  if M.options.show_index then
+    tab.string = tab.string .. tab.index
+  end
+
+  -- modify indicator
+  if bufmodified == 1 and M.options.show_modify then
+    tab.string = tab.string .. M.options.modify_indicator
+  else
+    tab.string = tab.string .. ' '
+  end
+
+  -- icon
+  local icon = ''
+  if M.options.show_icon and M.has_devicons then
+    local ext = fn.fnamemodify(bufname, ':e')
+    icon = M.devicons.get_icon(bufname, ext, { default = true }) .. ' '
+  end
+
+  -- buffer name
+  if bufname ~= '' then
+    local name = ''
+
+    local filename = fn.fnamemodify(bufname, M.options.fnamemodify)
+    local filename_length = utf8.len(filename)
+
+    if M.options.tab_max_length > 1 then
+      filename = M.options.shorten_indicator .. utf8.sub(filename, filename_length - M.options.tab_max_length + 2)
+    else
+      local tail = fn.fnamemodify(bufname, ':t')
+      local tail_length = utf8.len(tail)
+
+      local head = utf8.sub(filename, 1, filename_length - tail_length)
+      local head_length = utf8.len(head)
+
+      if head_length > 0 then
+        if M.options.shorten_path_fully then
+          name = M.options.shorten_indicator .. M.consts.path_separator
+        elseif M.options.shorten_path then
+          name = fn.pathshorten(head)
         else
-            s = s .. '%#TabLine#'
+          name = head
         end
-        -- tab index
-        s = s .. ' '
-        -- index
-        if options.show_index then
-            s = s .. index .. ':'
-        end
-        -- icon
-        local icon = ''
-        if options.show_icon and M.has_devicons then
-            local ext = fn.fnamemodify(bufname, ':e')
-            icon = M.devicons.get_icon(bufname, ext, { default = true }) .. ' '
-        end
-        -- buf name
-        s = s .. options.brackets[1]
-        local pre_title_s_len = string.len(s)
-        if bufname ~= '' then
-            s = s .. icon .. fn.fnamemodify(bufname, options.fnamemodify)
+      end
+
+      if M.options.filename_max_length > 1 then
+        if tail_length > M.options.filename_max_length then
+          name = name .. M.options.shorten_indicator .. utf8.sub(tail, tail_length - M.options.filename_max_length + 2)
         else
-            s = s .. options.no_name
+          name = name .. tail
         end
-        if
-            options.inactive_tab_max_length
-            and options.inactive_tab_max_length > 0
-            and index ~= fn.tabpagenr()
-        then
-            s = string.sub(
-                s,
-                1,
-                pre_title_s_len + options.inactive_tab_max_length
-            )
-        end
-        s = s .. options.brackets[2]
-        -- modify indicator
-        if
-            bufmodified == 1
-            and options.show_modify
-            and options.modify_indicator ~= nil
-        then
-            s = s .. options.modify_indicator
-        end
-        -- additional space at the end of each tab segment
-        s = s .. ' '
+      else
+        name = name .. tail
+      end
     end
 
-    s = s .. '%#TabLineFill#'
-    return s
+    if utf8.len(icon) > 0 then
+      tab.string = tab.string .. name .. ' ' .. icon
+    else
+      tab.string = tab.string .. name
+    end
+  else
+    tab.string = tab.string .. M.options.no_name
+  end
+
+  if tab.is_active then
+    tab.string = tab.string .. M.options.active_suffix
+  else
+    tab.string = tab.string .. M.options.suffix
+  end
+
+  tab.string_length = utf8.len(tab.string)
+end
+
+local function HandleFirstTab(s_length, available_width)
+  M.pages.alignment = 0
+
+  if s_length < available_width then
+    M.pages.view_begin_index = 1
+    M.pages.view_end_index = s_length
+    return
+  end
+
+  M.pages.view_begin_index = 1
+  M.pages.view_end_index = available_width
+end
+
+local function HandleLastTab(s_length, available_width)
+  M.pages.alignment = 1
+
+  if s_length < available_width then
+    M.pages.view_begin_index = 1
+    M.pages.view_end_index = s_length
+    return
+  end
+
+  M.pages.view_begin_index = s_length - available_width + 1
+  M.pages.view_end_index = s_length
+end
+
+local function HandleMiddleTab(s, s_length, available_width, tab)
+  if s_length < available_width then
+    M.pages.view_begin_index = 1
+    M.pages.view_end_index = s_length
+    return
+  end
+
+  local to_right = M.pages.active_tab_index > M.pages.previous_active_tab_index;
+  local to_left = M.pages.active_tab_index < M.pages.previous_active_tab_index;
+
+  if to_right then
+    local offset = 0
+    if M.pages.have_right_scroller then
+      offset = offset + M.consts.scroll_right_length
+    end
+
+    if tab.absolute_end_index < M.pages.view_end_index - offset then
+      return
+    end
+
+    M.pages.alignment = 1
+
+    M.pages.view_end_index = tab.absolute_end_index
+    M.pages.view_begin_index = tab.absolute_end_index - available_width + 1
+
+    return
+  end
+  
+  if to_left then
+    local offset = 0
+    if M.pages.have_left_scroller then
+      offset = offset + M.consts.scroll_left_length
+    end
+
+    if tab.absolute_begin_index > M.pages.view_begin_index + offset then
+      return
+    end
+
+    M.pages.alignment = 0
+
+    M.pages.view_begin_index = tab.absolute_begin_index
+    M.pages.view_end_index = tab.absolute_begin_index + available_width - 1
+
+    return
+  end
+
+  return
+end
+
+local function GetTabline(options)
+    local s = ''
+
+    local nr_tabs = fn.tabpagenr('$')
+
+    local begin_index = 1
+
+    local s_length = 0
+
+    for index = 1, nr_tabs do
+      local tab = M.pages:get(index)
+
+      BuildBufferName(tab)
+
+      -- TODO(tom): Ineffective. Re-factor.
+      if index == nr_tabs then
+        s = s .. tab.string
+
+        tab.absolute_begin_index = begin_index
+        tab.absolute_end_index = tab.absolute_begin_index + tab.string_length - 1
+
+        s_length = s_length + tab.string_length
+      else
+        s = s .. tab.string .. M.options.separator
+
+        tab.absolute_begin_index = begin_index
+        tab.absolute_end_index = tab.absolute_begin_index + tab.string_length - 1
+        
+        begin_index = tab.absolute_end_index + M.consts.separator_length + 1
+
+        s_length = s_length + tab.string_length + M.consts.separator_length
+      end
+    end
+
+    local width = vim.api.nvim_eval("&columns")
+
+    local available_width = width - M.consts.unavailable_length
+
+    -- number of tabs
+    local s_nr_tabs = ''
+    local s_nr_tabs_length = 0
+
+    if M.options.show_nr_tabs then
+      if s_length < available_width then
+        s_nr_tabs = s_nr_tabs .. '%#TabLineFill#%='
+      end
+      s_nr_tabs = s_nr_tabs .. ' [' .. M.pages.count .. ']'
+      s_nr_tabs_length = utf8.len(s_nr_tabs)
+      available_width = available_width - s_nr_tabs_length
+    end
+
+    -- view
+    local active_tab = M.pages:get_active()
+    if active_tab == nil then
+      return s
+    end
+
+    if active_tab.index == 1 then
+      HandleFirstTab(s_length, available_width)
+    elseif active_tab.index == nr_tabs then
+      HandleLastTab(s_length, available_width)
+    else
+      HandleMiddleTab(s, s_length, available_width, active_tab)
+    end
+
+    -- scrollers
+    local offset = 0
+    if M.options.show_scrollers then
+      if M.pages.view_begin_index > 1 then
+        if M.pages.view_end_index < s_length then
+          M.pages.have_left_scroller = true
+          M.pages.have_right_scroller = true
+          local need = M.consts.scroll_left_length + M.consts.scroll_right_length
+          if M.pages.alignment == 0 then
+            offset = M.consts.scroll_left_length
+            s = M.options.scroll_left .. utf8.sub(s, M.pages.view_begin_index, M.pages.view_end_index - need) .. M.options.scroll_right
+          else
+            offset = M.consts.scroll_left_length - need
+            s = M.options.scroll_left .. utf8.sub(s, M.pages.view_begin_index + need, M.pages.view_end_index) .. M.options.scroll_right
+          end
+        else
+          M.pages.have_left_scroller = true
+          M.pages.have_right_scroller = false
+          s = M.options.scroll_left .. utf8.sub(s, M.pages.view_begin_index + M.consts.scroll_left_length)
+        end
+      else
+        if M.pages.view_end_index < s_length then
+          M.pages.have_left_scroller = false
+          M.pages.have_right_scroller = true
+          local need = M.consts.scroll_right_length
+          s = utf8.sub(s, 1, M.pages.view_end_index - need) .. M.options.scroll_right
+        else
+          M.pages.have_left_scroller = false
+          M.pages.have_right_scroller = false
+          s = utf8.sub(s, M.pages.view_begin_index, M.pages.view_end_index)
+        end
+      end
+    else
+      s = utf8.sub(s, M.pages.view_begin_index, M.pages.view_end_index)
+    end
+
+    -- number of tabs
+    s = s .. s_nr_tabs
+
+    -- decorate
+    local position = active_tab.absolute_begin_index - M.pages.view_begin_index + offset
+    s = utf8.insert(s, '%#TabLine#', position + active_tab.string_length)
+    s = utf8.insert(s, '%' .. active_tab.index .. 'T' .. '%#TabLineSel#', position)
+
+    return M.options.header .. s .. M.options.footer
 end
 
 function M.setup(user_options)
-    M.options = vim.tbl_extend('force', M.options, user_options)
-    M.has_devicons, M.devicons = pcall(require, 'nvim-web-devicons')
+  M.options = vim.tbl_extend('force', M.options, user_options)
+  M.has_devicons, M.devicons = pcall(require, 'nvim-web-devicons')
 
-    function _G.nvim_tabline()
-        return tabline(M.options)
-    end
+  M.consts.separator_length = utf8.len(M.options.separator)
+  M.consts.scroll_left_length = utf8.len(M.options.scroll_left)
+  M.consts.scroll_right_length = utf8.len(M.options.scroll_right)
+  M.consts.header_length = utf8.len(M.options.header)
+  M.consts.footer_length = utf8.len(M.options.footer)
+  M.consts.unavailable_length = M.consts.header_length + M.consts.footer_length
 
-    vim.o.showtabline = 2
-    vim.o.tabline = '%!v:lua.nvim_tabline()'
+  function _G.nvim_tabline()
+    return GetTabline(M.options)
+  end
 
-    vim.g.loaded_nvim_tabline = 1
+  vim.o.showtabline = 2
+  vim.o.tabline = '%!v:lua.nvim_tabline()'
+
+  vim.g.loaded_nvim_tabline = 1
 end
 
 return M
